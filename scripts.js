7fd6295fb690cbeaae005131fbd6c4385389e318
jklxco <jack.a.coleman@hotmail.com>
2023-07-28 12:44:54 +0100

    Added button to customise grid dimensions

--- scripts.js
+++ scripts.js
@@ -1,16 +1,20 @@
-function createGameBoard() {
+function createGameBoard(height = 16, width = 16, ) {
     const containerDiv = document.querySelector('.container');
+    containerDiv.replaceChildren()
     
-    for (i = 0; i < 16; i++) {
+    for (i = 0; i < width; i++) {
        let rowDiv = document.createElement('div');
        rowDiv.classList.add('row-div');
-       for (j = 0; j < 16; j++) {
+       for (j = 0; j < height; j++) {
             let cellDiv = document.createElement('div');
             cellDiv.classList.add('cell');
+            cellDiv.style.height = `${400/height}px`
+            cellDiv.style.width = `${400/width}px`
             rowDiv.appendChild(cellDiv)
        } 
        containerDiv.appendChild(rowDiv);
     }
+    addEventListener()
 }
 
 function addEventListener(){
@@ -20,5 +24,14 @@ function addEventListener(){
     });
 }
 
+function changeGridSize(){
+    const btnChangeGrid = document.getElementById('btn-change-grid')
+    btnChangeGrid.addEventListener('click', function(){
+        let height = prompt('How Many Squares Tall?')
+        let width = prompt('How Many Squares Wide?')
+        createGameBoard(height, width)
+    })
+}
+
 createGameBoard()
-addEventListener()
+changeGridSize()
